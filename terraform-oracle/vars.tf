916789573e782b3621ef6fbbbdabb807be8c2cab
amcbarnett <mckingdom@Ancils-MacBook-Pro.local>
2018-04-25 15:58:59 -0400

    Added TFE IP

---
+++ terraform-oracle/vars.tf
@@ -0,0 +1,45 @@
+variable "count_instances" {
+  default = 3
+}
+
+variable "name" {
+  description = "name to pass to Name tag"
+  default = "Provisioned by Terraform"
+}
+
+variable "SSH_KEY_NAME" {
+  default = "cloudbreakkeypair"
+  description = "Pre-existing AWS key name you will use to access the instance(s)"
+}
+
+variable "INSTANCE_TYPE" {
+  default = "t2.micro"
+}
+
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "PRIVATE_KEY" {
+  default = ""
+}
+
+
+variable "ami_id" {
+  description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
+  default = "ami-2e1ef954"
+}
+
+
+variable "MY_IP" {
+  description = "Enter your Public IP like thos x.x.x.x/32"
+}
+
+variable "TFE_IP" {
+  default = "0.0.0.0/0"
+  description = "Enter the TFE IP"
+}
+
+output "public_ips" {
+ value = ["${aws_instance.oraclerac.*.public_ip}"]
+}
