c1b08e7e8dc82e6ade758ff1fbeee8e2322419e9
amcbarnett <8419906+amcbarnett@users.noreply.github.com>
2018-04-27 05:54:04 -0400

    Update Deploy Cluster

--- terraform-oracle/scripts/deploycluster.sh
+++ terraform-oracle/scripts/deploycluster.sh
@@ -3,8 +3,11 @@
 cd $1
 
 ./bin/alluxio bootstrapConf $2
+cp conf/alluxio-site.properties.template cond/alluxio-site.properties
 ./bin/alluxio copyDir conf
 
 ./bin/alluxio format
 ./bin/alluxio-start.sh all SudoMount
-./bin/alluxio runTests
+
+#sleep 15s
+#./bin/alluxio runTests
