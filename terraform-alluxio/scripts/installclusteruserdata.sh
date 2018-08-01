b9635b4532d1aa1c5999117675616d102d7d65db
amcbarnett <8419906+amcbarnett@users.noreply.github.com>
2018-07-31 23:02:57 -0400

    Added Daat template file

---
+++ terraform-alluxio/scripts/installclusteruserdata.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo yum install -y java-1.8.0-openjdk-devel
+
+curl -Ls http://downloads.alluxio.org/downloads/files/1.7.1/alluxio-1.7.1-hadoop-2.8-bin.tar.gz | tar -xz 
+
+mv alluxio-1.7.1-hadoop-2.8 ${name}
