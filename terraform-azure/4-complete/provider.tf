4a60e26d5651c449220827a177f7b73e5fe88ed3
amcbarnett <8419906+amcbarnett@users.noreply.github.com>
2018-09-18 13:36:54 -0400

    test Vault

--- terraform-azure/4-complete/provider.tf
+++ terraform-azure/4-complete/provider.tf
@@ -1,7 +1,29 @@
-provider "azurerm" {
+/*provider "azurerm" {
   /*subscription_id = ""
   client_id       = ""
   client_secret   = ""
   tenant_id       = ""
-  */
+  
+}*/
+
+provider "azurerm" {
+  subscription_id = "${data.vault_generic_secret.cyberark.data.subscription_id["Value"]}"
+  client_id       = "${data.vault_generic_secret.cyberark.data.client_id["Value"]}"
+  client_secret   = "${data.vault_generic_secret.cyberark.data.client_secret["Value"]}"
+  tenant_id       = "${data.vault_generic_secret.cyberark.data.tenant_id["Value"]}"
+}
+
+provider "vault" {
+  # It is strongly recommended to configure this provider through the
+  # environment variables described above, so that each user can have
+  # separate credentials set in the environment.
+  #
+  # This will default to using $VAULT_ADDR
+  # But can be set explicitly
+  # address = "https://vault.example.net:8200"
+  # set VAULT_TOKEN as a sensitive variable also to access Cybera=Ark secret engine
+}
+
+data "vault_generic_secret" cyberark {
+  path = "secret/ancil/azure/credentials"
 }
