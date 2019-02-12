ae1fe16a106f196feaea9e83e5d8611c72dafdec
amcbarnett <8419906+amcbarnett@users.noreply.github.com>
2019-02-12 02:28:55 -0500

    Create 4. create-vault-service.sh

---
+++ vault-gke-yaml/4. create-vault-service.sh
@@ -0,0 +1,19 @@
+cat <<EOF > vaultservice.yaml
+apiVersion: v1
+kind: Service
+metadata:
+  name: vault
+  labels:
+    app: vault
+spec:
+  type: ClusterIP
+  ports:
+    - name: vault
+      port: 8200
+      targetPort: 8200
+      protocol: "TCP"
+  selector:
+    app: vault
+EOF
+
+kubectl create -f vaultservice.yaml -n vault-deploy
