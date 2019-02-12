c9df895eb285155e33bd1f6a8a18afa10165405d
amcbarnett <8419906+amcbarnett@users.noreply.github.com>
2019-02-12 03:24:57 -0500

    Create 5. vault-k8s-auth.sh

---
+++ vault-gke-yaml/5. vault-k8s-auth.sh
@@ -0,0 +1,234 @@
+
+
+#Initialize Vault
+
+VAULT_POD=$(kubectl get pods --namespace vault-deploy -l "app=vault" -o jsonpath="{.items[0].metadata.name}")
+VAULT_SERVICE=$(kubectl get svc -l app=vault -o jsonpath="{.items[0].metadata.name}")
+
+## Using Command Line
+#kubectl exec -it vault-0 -c vault -- /bin/sh
+
+#kubectl exec -it ${VAULT_POD} -c ${VAULT_SERVICE} -- /bin/sh export VAULT_ADDR=http://127.0.0.1:8200
+
+kubectl exec -it ${VAULT_POD} -c ${VAULT_SERVICE} -- /bin/sh -c 'VAULT_ADDR=http://localhost:8200 vault operator init -key-shares=1 -key-threshold=1'
+
+#Unseal Key 1: xx
+
+#Initial Root Token: xx
+
+export VAULT_UNSEAL="xxx"
+export VAULT_TOKEN="xxx"
+
+for v in `kubectl get pods | grep vault | cut -f1 -d' '`; do kubectl exec -ti $v -c vault -- /bin/sh -c 'VAULT_ADDR=http://localhost:8200 vault operator unseal bPV4zv3ezMR29ipBPW8jDfHh7CjsIolRSoiPBOMHhls=
+'; done
+
+for v in `kubectl get pods | grep vault | cut -f1 -d' '`; do kubectl exec -ti $v -c vault -- /bin/sh -c 'VAULT_ADDR=http://localhost:8200 vault status'; done
+
+
+#export VAULT_TOKEN=$(kubectl logs $VAULT_POD vault | grep 'Root Token' | cut -d' ' -f3)
+
+export VAULT_ADDR=http://127.0.0.1:8200
+
+# run this on a second terminal
+kubectl port-forward $VAULT_POD 8200
+
+echo $VAULT_TOKEN | vault login -
+vault status
+
+-------------------------------------------
+
+Creating a Vault policy to a specific role:
+$ cat > postgres-policy.hcl <<EOF
+path "database/creds/postgres-role" {
+  capabilities = ["read"]
+}
+path "sys/leases/renew" {
+  capabilities = ["create"]
+}
+path "sys/leases/revoke" {
+  capabilities = ["update"]
+}
+EOF
+$ vault policy write postgres-policy postgres-policy.hcl
+Success! Uploaded policy: postgres-policy
+
+-------------------------------------------
+
+#1. Create Kubernetes Service Account
+
+# A service account can have multiple namespaces
+# kubectl create serviceaccount vault-auth
+
+#kubectl create clusterrolebinding add-on-cluster-admin --clusterrole=cluster-admin --serviceaccount=kube-system:default
+#kubectl create clusterrolebinding role-tokenreview-binding --clusterrole=system:auth-delegator --serviceaccount=k8s-app1:vault-app
+
+# 2. Grant service account ability to access the TokenReviewer API via RBAC
+$ cat > vault-serviceaccount.yml <<EOF
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: vault-auth
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: role-tokenreview-binding
+  namespace: vault-deploy
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: system:auth-delegator
+subjects:
+- kind: ServiceAccount
+  name: vault-auth
+  namespace: vault-deploy
+EOF
+
+kubectl apply -f vault-serviceaccount.yml
+
+
+-----------------
+
+
+
+# 3. Enable Kubernetes Auth Method
+export LB_IP="$(gcloud compute addresses describe vault --region us-east1 --format 'value(address)')"
+
+export GOOGLE_CLOUD_PROJECT="mckingdom-gcp"
+export GOOGLE_ZONE="us-east4-a"
+export GOOGLE_CLUSTER="k8s-mcb"
+
+export CLUSTER_NAME="gke_${GOOGLE_CLOUD_PROJECT}_${GOOGLE_ZONE}_${GOOGLE_CLUSTER}"
+
+#Note: Command for all clusters: 
+# kubectl config view -o jsonpath='{range .clusters[*]}{.name}{"\t"}{.cluster.server}{"\n"}{end}'
+
+export VAULT_SA_NAME="$(kubectl get serviceaccount vault-auth -o go-template='{{ (index .secrets 0).name }}')"
+export SA_JWT_TOKEN="$(kubectl get secret ${VAULT_SA_NAME} -o go-template='{{ .data.token }}' | base64 --decode)"
+export K8S_HOST="$(kubectl config view --raw \
+    -o go-template="{{ range .clusters }}{{ if eq .name \"${CLUSTER_NAME}\" }}{{ index .cluster \"server\" }}{{ end }}{{ end }}")"
+export K8S_CACERT="$(kubectl config view --raw \
+    -o go-template="{{ range .clusters }}{{ if eq .name \"${CLUSTER_NAME}\" }}{{ index .cluster \"certificate-authority-data\" }}{{ end }}{{ end }}" | base64 --decode)"
+
+export VAULT_SA_NAME=$(kubectl get serviceaccount vault-auth -o jsonpath="{.secrets[*]['name']}")
+export SA_JWT_TOKEN=$(kubectl get secret ${VAULT_SA_NAME} -o jsonpath="{.data.token}" | base64 --decode; echo)
+export K8S_CACERT=$(kubectl get secret ${VAULT_SA_NAME} -o jsonpath="{.data['ca\.crt']}" | base64 --decode; echo)
+export K8S_HOST=$(kubectl exec -it vault-0 -c vault -- /bin/sh -c 'echo $KUBERNETES_SERVICE_HOST')
+
+
+export VAULT_SERVICE=$(kubectl get svc -l app=vault -o jsonpath="{.items[0].metadata.name}")
+
+
+echo ${SA_JWT_TOKEN}
+echo ${K8S_HOST}
+echo ${K8S_CACERT}
+
+---
+
+kubectl exec -it vault-0 -c vault -- /bin/sh
+
+export VAULT_ADDR=http://127.0.0.1:8200
+export VAULT_TOKEN="s.dj0373jLLXaxOndDVzui2yeI"
+
+vault auth enable kubernetes
+
+vault write auth/kubernetes/config \
+    kubernetes_host="${K8S_HOST}" \
+    kubernetes_ca_cert="${K8S_CACERT}" \
+    token_reviewer_jwt="${SA_JWT_TOKEN}"
+
+vault write auth/kubernetes/config \
+  token_reviewer_jwt="${SA_JWT_TOKEN}" \
+  kubernetes_host="https://10.55.240.1:443" \
+  kubernetes_ca_cert="${K8S_CACERT}"
+
+
+-----------------
+
+#Create service accounts for two apps/ processes in a new name space vault-app
+
+kubectl create serviceaccount k8s-app1
+kubectl create serviceaccount k8s-app2
+
+
+kubectl create ns vault-app1
+kubectl create ns vault-app2
+
+kubectl get serviceAccounts
+kubectl get ns
+
+vault write auth/kubernetes/role/testauth \
+    bound_service_account_names=vault-auth \
+    bound_service_account_namespaces=vault-deploy \
+    policies=test-policy \
+    ttl=24h
+
+
+vault write auth/kubernetes/role/app1 \
+    bound_service_account_names=vault-auth \
+    bound_service_account_namespaces=vault-app1 \
+    policies=test-policy2 \
+    ttl=24h
+
+
+vault write auth/kubernetes/role/app2 \
+    bound_service_account_names=vault-auth \
+    bound_service_account_namespaces=vault-app2 \
+    policies=test-policy3 \
+    ttl=24h
+
+$ cat > vault-app1tokenreview.yml <<EOF
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: vault-app1-tokenreview-binding
+  namespace: vault-app1
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: system:auth-delegator
+subjects:
+- kind: ServiceAccount
+  name: vault-auth
+  namespace: vault-app1
+EOF
+
+#For all service accounts in the "qa" namespace"
+subjects:
+- kind: Group
+  name: system:serviceaccounts:qa
+  apiGroup: rbac.authorization.k8s.io
+
+kubectl apply -f vault-app1tokenreview.yml 
+
+kubectl config set-context vault-app1 --namespace=vault-app1 \
+  --cluster=gke_mckingdom-gcp_us-east4-a_k8s-mcb \
+  --user=gke_mckingdom-gcp_us-east4-a_k8s-mcb
+
+kubectl config use-context vault-app1
+
+
+---------------------
+
+To test
+Create a Temporary Image:
+
+kubectl run --generator=run-pod/v1 tmp  -i --tty --serviceaccount=vault-auth --image alpine
+
+kubectl attach tmp -c tmp -i -t
+
+# some preq
+$ apk update
+$ apk add curl postgresql-client jq
+# fetch the vault token of this specific pod
+
+export KUBE_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+echo ${KUBE_TOKEN}
+
+export VAULT_K8S_LOGIN=$(curl --request POST --data '{"jwt": "'"$KUBE_TOKEN"'", "role": "testauth"}' http://vault:8200/v1/auth/kubernetes/login)
+
+echo $VAULT_K8S_LOGIN | jq
+
+X_VAULT_TOKEN=$(echo $VAULT_K8S_LOGIN | jq -r '.auth.client_token')
+
+
