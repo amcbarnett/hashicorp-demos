867e255da0302b8a1b65e27a945a589ed59eb103
amcbarnett <8419906+amcbarnett@users.noreply.github.com>
2019-02-12 02:26:04 -0500

    Update 2. deploy_consul_yaml.sh

--- vault-gke-yaml/2. deploy_consul_yaml.sh
+++ vault-gke-yaml/2. deploy_consul_yaml.sh
@@ -1,19 +1,25 @@
 #Create consul service in namespace vault-deploy
+#Review consulservice.yaml first
 
 kubectl create -f consulservice.yaml -n vault-deploy
 
 #Install 3 Pods for Consul to run in
+#Review onsulstatefulset.yaml first
 kubectl create -f consulstatefulset.yaml -n vault-deploy
 
 #Command to delete pods
 #kubectl delete -f consulstatefulset.yaml
 
 kubectl get service
+
+#Keep executing until three pods created and running
 kubectl get pods
 
 #Test that Consul is working
 #In another Terminal on local mac forward port
 kubectl port-forward consul-1 8500:8500
+
+#Go to web for Console UI
 
 #Go back to previous terminal and run
 consul members
