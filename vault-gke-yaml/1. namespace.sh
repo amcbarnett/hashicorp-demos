496cb8847bfdbe05daf34452e4e5c0bc11001b60
amcbarnett <8419906+amcbarnett@users.noreply.github.com>
2019-02-12 01:36:32 -0500

    Create 1. namespace.sh

---
+++ vault-gke-yaml/1. namespace.sh
@@ -0,0 +1,16 @@
+kubectl create ns vault-deploy
+kubectl config view
+kubectl config current-context
+
+export GKE_CLUSTER=gke_mckingdom-gcp_us-east4-a_k8s-mcb
+export GKE_USER=gke_mckingdom-gcp_us-east4-a_k8s-mcb
+
+kubectl config set-context vault-deploy --namespace=vault-deploy \
+  --cluster=${GKE_CLUSTER} \
+  --user={GKE_USER}
+
+#Switch Context
+kubectl config use-context vault-deploy 
+
+#To Delete Context
+#kubectl config delete-context vault-deploy
