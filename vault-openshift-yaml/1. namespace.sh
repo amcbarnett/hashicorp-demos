74fb6b0988efc1db8be97836e5091dcd4da1048a
amcbarnett <8419906+amcbarnett@users.noreply.github.com>
2019-03-18 11:30:56 -0400

    Create 1. namespace.sh

---
+++ vault-openshift-yaml/1. namespace.sh
@@ -0,0 +1,12 @@
+oc create ns vault-deploy --config=/etc.origin/mater/admin.kubecofig
+oc get project
+
+
+#Switch Context
+oc config use-context vault-deploy 
+
+# List Contexts
+oc config current-context
+
+#To Delete Context
+#kubectl config delete-context vault-deploy
